b0102fb33f06fc539bf65c764a297f26de2f1ff3
Alberto Rosa <alberto.r.93@hotmail.it>
2022-04-09 20:24:30 +0200

    stamp funzionante

--- csharp-oop-shop/Classi.cs
+++ csharp-oop-shop/Classi.cs
@@ -6,66 +6,111 @@ using System.Threading.Tasks;
 
 namespace prodotti_class
 {
-    public class Prodotto
+    internal class Prodotto
     {
         private int codice;
         private string nome;
         private string descrizione;
         private float prezzo;
-        private byte iva;
-
-
+        private int iva;
 
 
         //costruttore
-        public Prodotto(int codice, string nome, string descrizione, float prezzo, byte iva)
+        public Prodotto(string nome, string descrizione, float prezzo, int iva)
         {
             //this si riferisce all'attuale uso del nome nella class
-            this.codice = codice;
+            this.codice = CreaCodice();
             this.nome = nome;
             this.descrizione = descrizione;
             this.prezzo = prezzo;
             this.iva = iva;
         }
 
-        //metodo di aggiunta 
-        public void AggiungiOggetto ()
+
+        //printer
+
+        public void StampaNome()
         {
-            Console.WriteLine("------- Metodo di creazione prodotto --------");
-            Console.Write("Il prodotto aggiunto avrà codice: ");
-            codice = CreaCodice();
-
-            Console.Write("Inserisci il nome prodotto: ");
-            nome = Console.ReadLine();
-
-            Console.Write("Inserisci la descrizione del prodotto: ");
-            descrizione = Console.ReadLine();
-
-            Console.Write("Inserisci il codice prodotto: ");
-            codice = Int32.Parse(Console.ReadLine());
-
+            Console.WriteLine("NOME PRODOTTO: " + nome);
         }
 
-        //metodo creacoride casuale
+        public void StampaCodice()
+        {
+            Console.WriteLine("CODICE - " + codice);
+        }
+
+        public void StampaDescrizione()
+        {
+            Console.WriteLine(" > " + descrizione);
+        }
+
+        public void StampaPrezzo()
+        {
+            Console.WriteLine(prezzo + "&euros;");
+        }
+
+
+
+
+
+
+
+
+        public void StampaProdotto()
+        {
+            Console.WriteLine("--------------------------------");
+            StampaNome();
+            StampaCodice();
+            StampaDescrizione();
+            StampaPrezzo();
+            Console.WriteLine("--------------------------------");
+        }
+
+
+
+
+
+        //metodo di aggiunta 
+ 
+
+
+
+
+
+
+
+
+
+
+        //setter: metodi che permettono di inserire i dati senza che escano dai canoni scelti
+
+
+
+
+
+
+
+        //---------------------------------------------------------------
+
+        //metodo creacodice casuale
         private int CreaCodice()
         {
             //inizializzo la classe con un costruttore di rng
             Random random = new Random();
             return random.Next(9999); //compreso tra
         }
-
-}
+    }
 
 }
 
 
 /*
 Usate opportunamente ai modificatori di accesso (public, private), i costruttori, i metodi getter e setter ed eventuali altri metodi di “utilità” per fare in modo che:
--alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random.
-- Il codice prodotto sia accessibile solo in lettura
-- Gli altri attributi siano accessibili sia in lettura che in scrittura
-- Il prodotto esponga sia un metodo per avere il prezzo base
-- uno per avere il prezzo comprensivo di iva
-- Il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice + nome
+-alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random. ----------------done
+- Il codice prodotto sia accessibile solo in lettura-------------------------------------------------
+- Gli altri attributi siano accessibili sia in lettura che in scrittura------------------------------
+- Il prodotto esponga sia un metodo per avere il prezzo base-----------------------------------------
+- uno per avere il prezzo comprensivo di iva---------------------------------------------------------
+- Il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice + nome--------
 */
 
--- csharp-oop-shop/Program.cs
+++ csharp-oop-shop/Program.cs
@@ -4,7 +4,41 @@
 Nel vostro programma principale, testate tutte le funzionalità della classe Prodotto, creando vari prodotti e stampandone il loro contenuto.
 */
 
+using prodotti_class;
 
 
 
+
+Console.WriteLine("Programma per aggiungere Prodotti al database");
+
+while (true)
+{
+
+    Console.WriteLine("------- Metodo di creazione prodotto --------");
+
+    Console.Write("Inserisci il nome prodotto: ");
+    string nome = Console.ReadLine();
+
+    Console.Write("Inserisci la descrizione del prodotto: ");
+    string descrizione = Console.ReadLine();
+
+    Console.Write("Inserisci il prezzo originale: ");
+    float prezzo = Int32.Parse(Console.ReadLine());
+
+    Console.Write("Inserisci l'iva in percentuale: ");
+    int iva = Int32.Parse(Console.ReadLine());
+
+
+    Prodotto oggetto1 = new Prodotto(nome, descrizione, prezzo, iva);
+
+    oggetto1.StampaProdotto();
+
+
+
+
+
+
+
+
+}
 
